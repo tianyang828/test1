88dcbdaca6dc3ba47a694b3cec589729fb145e12
tianyang <tianyang6916@163.com>
2018-11-22 11:30:00 +0800

    修改spec

---
+++ test1/test1.framework/Headers/Father.h
@@ -0,0 +1,20 @@
+//
+//  Father.h
+//  test1
+//
+//  Created by 田洋 on 2018/11/20.
+//  Copyright © 2018 田洋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Father : NSObject
+
+
+- (void)talk;
+
+@end
+
+NS_ASSUME_NONNULL_END
